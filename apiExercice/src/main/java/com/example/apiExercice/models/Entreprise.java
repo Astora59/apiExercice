71e0736010fc2f896fc62a0cfa6dcd63ba3cec4b
Yamin <yaminmoulay12@gmail.com>
2025-08-12 11:14:04 +0200

    added attributes to Entreprise class, added getter and setter to access them outside the class

--- apiExercice/src/main/java/com/example/apiExercice/models/Entreprise.java
+++ apiExercice/src/main/java/com/example/apiExercice/models/Entreprise.java
@@ -1,4 +1,33 @@
 package com.example.apiExercice.models;
 
+import java.util.UUID;
+
 public class Entreprise {
+    private UUID id;
+    private String name;
+    private String adresse;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 }
