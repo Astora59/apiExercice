9726aee5f7f42ec11930c31c4752e807dbeccc0c
Yamin <yaminmoulay12@gmail.com>
2025-08-12 10:35:51 +0200

    first commit

---
+++ apiExercice/src/main/java/com/example/apiExercice/ApiExerciceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiExercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExerciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExerciceApplication.class, args);
+	}
+
+}
