5bd78ea3fed9f654fd30b33515846857525372da
Yamin <yaminmoulay12@gmail.com>
2025-08-12 11:36:14 +0200

    annotations added : @table to turn it into a table, @entity, @Id and @GeneratedValue for id attribute

--- apiExercice/src/main/java/com/example/apiExercice/models/Entreprise.java
+++ apiExercice/src/main/java/com/example/apiExercice/models/Entreprise.java
@@ -1,12 +1,21 @@
 package com.example.apiExercice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+@Table(name="entreprise")
+@Entity
 public class Entreprise {
     private UUID id;
     private String name;
     private String adresse;
 
+    @Id
+    @GeneratedValue
     public UUID getId() {
         return id;
     }
