e8fe669473d10f1f57b8124a067a63081571922a
Yamin <yaminmoulay12@gmail.com>
2025-08-13 19:46:47 +0200

    Ajout du repository et de la configuration H2 pour connecter le service à la base et utiliser findAll() via CrudRepository

--- apiExercice/src/main/java/com/example/apiExercice/services/implementations/EntrepriseServiceImpl.java
+++ apiExercice/src/main/java/com/example/apiExercice/services/implementations/EntrepriseServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.apiExercice.services.implementations;
 
 import com.example.apiExercice.models.Entreprise;
+import com.example.apiExercice.repositories.EntrepriseRepository;
 import com.example.apiExercice.services.EntrepriseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,21 +12,14 @@ import java.util.UUID;
 
 @Service //indique que cette classe est un service qu'on peut injecter dans un autre composant
 public class EntrepriseServiceImpl implements EntrepriseService {
+
+    @Autowired
+    private EntrepriseRepository entrepriseRepository;
+
     @Override //indique que la classe va réécrire findAll
     public List<Entreprise> findAll() {
-        List<Entreprise> liste = new ArrayList<Entreprise>();
+        List<Entreprise> list = new ArrayList<Entreprise>();
 
-        Entreprise e1 = new Entreprise();
-        e1.setId(UUID.randomUUID()); //on attribue un UUID random
-        e1.setName("Entreprise Alpha");
-        e1.setAdresse("15 rue Victor Hugo, 59150 Wattrelos");
-        liste.add(e1);
-
-        Entreprise e2 = new Entreprise();
-        e2.setId(UUID.randomUUID()); //on attribue un UUID random
-        e2.setName("Entreprise  Beta");
-        e2.setAdresse("15 rue Guy de Maupassant, 59000 Lille");
-        liste.add(e2);
-        return liste;
+        entrepriseRepository.findAll();
     }
 }
